234c384967a5e56fffdc826637b6aafa8e3778a4
Garima Agarwal <ga@cover.co>
2014-08-11 15:37:26 -0700

    Initial Commit

---
+++ CameraView/GAViewController.m
@@ -0,0 +1,29 @@
+//
+//  GAViewController.m
+//  CameraView
+//
+//  Created by Garima Agarwal on 8/11/14.
+//  Copyright (c) 2014 Garima Agarwal. All rights reserved.
+//
+
+#import "GAViewController.h"
+
+@interface GAViewController ()
+
+@end
+
+@implementation GAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ CameraView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CameraView
+//
+//  Created by Garima Agarwal on 8/11/14.
+//  Copyright (c) 2014 Garima Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GAAppDelegate class]));
+    }
+}
